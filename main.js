0a3fcc606e91a55803f3e16cc371dac6cb5faf5a
schujack-ctrl <84467112+schujack-ctrl@users.noreply.github.com>
2022-01-31 14:11:18 -0600

    Initialize variables and pull data from data set
    Initialize list variables and pull data from data set

---
+++ main.js
@@ -0,0 +1,7 @@
+// Initilize list variables with data from the data set
+cityList = getColumn("Daily Weather", "City");
+forecastNumberList = getColumn("Daily Weather", "Forecast Number");
+conditionList = getColumn("Daily Weather", "Condition Description");
+iconList = getColumn("Daily Weather", "Icon");
+highTempList = getColumn("Daily Weather", "High Temperature");
+lowTempList = getColumn("Daily Weather", "Low Temperature");
